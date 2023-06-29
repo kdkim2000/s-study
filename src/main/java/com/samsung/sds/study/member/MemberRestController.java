a9a4e9a688c8202818be6614d2b2901163d5d377
kdkim2000 <kdkim2000@gmail.com>
2023-06-29 20:44:13 +0900

    1. Swagger 추가 2. updateMember 이름, e-mail 수정

--- src/main/java/com/samsung/sds/study/member/MemberRestController.java
+++ src/main/java/com/samsung/sds/study/member/MemberRestController.java
@@ -40,10 +40,21 @@ public class MemberRestController {
         member.setName(name);
         return memberRepository.save(member);
     }
-
-
-
-
-
+    @PutMapping("/{id}/email")
+    public Member updateMemberEmail(@PathVariable Long  id,
+                                    @RequestParam String email){
+        Member member = memberRepository.findById(id).get();
+        member.setEmail(email);
+        return memberRepository.save(member);
+    }
+    @PutMapping("/{id}")
+    public Member updateMember(@PathVariable Long  id,
+                               @RequestParam String name,
+                               @RequestParam String email){
+        Member member = memberRepository.findById(id).get();
+        member.setEmail(email);
+        member.setName(name);
+        return memberRepository.save(member);
+    }
 
 }
